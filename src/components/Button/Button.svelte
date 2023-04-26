4e113b3919e6fc8c2000df8f313c89d58d6dd951
AnnikenYT <anniken@mooonshine.net>
2023-04-26 11:37:18 -0300

    feat(Button): add Svelte button component
    ⚠️ Generated by a development version of GCM-AI.
    Results may be inaccurate.

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	// TODO style
+	const dispatch = createEventDispatcher();
+</script>
+
+<button on:click={() => dispatch('click')}>
+	<slot />
+</button>
