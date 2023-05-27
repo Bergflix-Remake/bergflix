6acadd05b68e19e8c744a46d578b26c6d25722d3
Leo P <anniken@bergflix.de>
2023-05-27 18:32:08 -0300

    chore(tailwind): add forms plugin and remove some colors
    ⚠️ Generated by a development version of GCM-AI.
    Results may be inaccurate.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,32 +9,8 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: {
-					50: "ffd9e0",
-					100: '#ffccd6',
-					200: '#ffc0cb',
-					300: '#ff9aac',
-					400: '#ff4e6e',
-					500: '#ff0230',
-					600: '#e6022b',
-					700: '#bf0224',
-					800: '#99011d',
-					900: '#7d0118'
-				},
 				'clean-white': '#FFFFFF',
 				'darkest-dark': '#000000',
-				'clean-dark': {
-					50: "dbdbdb",
-					100: '#cfcfcf',
-					200: '#c4c4c4',
-					300: '#a0a0a0',
-					400: '#585858',
-					500: '#111111',
-					600: '#0f0f0f',
-					700: '#0d0d0d',
-					800: '#0a0a0a',
-					900: '#080808'
-				},
 				delorean: {
 					100: '#e1e1e1',
 					200: '#c4c4c4',
@@ -46,7 +22,6 @@ module.exports = {
 					800: '#2b2b2b',
 					900: '#151515'
 				},
-				// 'concrete': '#989898',
 				concrete: {
 					100: '#eaeaea',
 					200: '#d6d6d6',
@@ -72,6 +47,7 @@ module.exports = {
 		}
 	},
 	plugins: [
+		require('@tailwindcss/forms'),
 		// 3. Append the Skeleton plugin to the end of this list
 		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
 	]
