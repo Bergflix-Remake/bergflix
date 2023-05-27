24e8f48e07cf68cf2cbba365e928908f67c0a112
Leo P <anniken@bergflix.de>
2023-05-27 18:47:52 -0300

    feat(dx): setup skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,30 @@
 <script>
-	import '../app.css';
 	import Navbar from '../components/Navbar/Navbar.svelte';
+	import { AppShell, Modal, Toast } from '@skeletonlabs/skeleton';
+
+	// Setup Skeleton's global styles:
+	import '../theme.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	// App Stylesheet
+	import '../app.css';
+
+	// FloatingUI
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<Navbar />
-<main>
+<!-- Overlays -->
+<Toast background="variant-filled-primary" position="bl" />
+<Modal />
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<Navbar />
+	</svelte:fragment>
 	<slot />
-</main>
+</AppShell>
 
 <style lang="postcss">
-	main {
-		@apply p-4 min-h-screen;
-	}
 </style>
