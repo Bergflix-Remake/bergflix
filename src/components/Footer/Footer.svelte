f00bbed7cd793076a554664d9cab6bb80da6c3f5
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2023-05-28 23:22:41 +0200

    feat: :sparkles: added Footer
    Footer and normal Links introduced.

---
+++ src/components/Footer/Footer.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import Link from '../Link.svelte';
+	import Logo from '../Logo.svelte';
+</script>
+
+<footer class="w-full p-8 shadow-inner bg-darkest-dark text-white pb-20 md:pb-8">
+	<div class="mb-4 sm:flex">
+		<div
+			class="h-auto text-3xl text-center sm:w-1/12 sm:text-sm md:text-base lg:text-lg xl:text-2xl"
+		>
+			<Logo />
+		</div>
+		<div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
+			<div class="mb-2 font-bold text-delorean">Social Media</div>
+			<ul class="leading-normal list-reset">
+				<li>
+					<Link icon="ExternalLinkIcon" href="https://bflx.de/instagram">Instagram</Link>
+				</li>
+				<li>
+					<Link icon="ExternalLinkIcon" href="https://bflx.de/discord">Discord</Link>
+				</li>
+				<li>
+					<Link icon="ExternalLinkIcon" href="https://bflx.de/twitter">Twitter</Link>
+				</li>
+			</ul>
+		</div>
+		<div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
+			<div class="mb-2 font-bold text-delorean">
+				<Logo gradient={true}>Brand</Logo>
+			</div>
+			<ul class="leading-normal list-reset">
+				<li>
+					<Link href="https://bflx.de/discord">Mithelfen</Link>
+				</li>
+				<li>
+					<Link icon="ExclamationIcon" href="https://bflx.de/bug-report">Fehler Melden</Link>
+				</li>
+			</ul>
+		</div>
+		<div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
+			<div class="mb-2 font-bold text-delorean">Rechtliches & Hilfe</div>
+			<ul class="leading-normal list-reset">
+				<!-- <li>
+                    <Link :to="{ name: 'legal', hash: '#privacy'}">
+                    Datenschutz<span class="hidden md:inline">erklärung</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link :to="{ name: 'legal', hash: '#imprint' }">
+                    Impressum
+                    </Link>
+                </li>
+                <li>
+                    <Link :to="{ name: 'legal', hash: '#terms' }">
+                    Benutzungsbedingungen
+                    </Link>
+                </li> -->
+			</ul>
+		</div>
+		<div class="h-auto mt-8 sm:w-1/4 sm:mt-0">
+			<div class="mb-2 font-bold text-delorean">Mehr von "Herr Bergmann"</div>
+			<ul class="leading-normal list-reset">
+				<li>
+					<Link icon="ExternalLinkIcon" href="https://discord.gg/fromthefuture">
+						Trete der “FTF”-Redaktion bei
+					</Link>
+				</li>
+				<li>
+					<Link icon="ExternalLinkIcon" href="https://www.patreon.com/herrbergmann">
+						Unterstütze seine Projekte
+					</Link>
+				</li>
+			</ul>
+		</div>
+	</div>
+</footer>
---
+++ src/components/Link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let href = '';
+	export let icon = '';
+</script>
+
+<a
+	{href}
+	class="font-bold text-white ease-in-out cursor-pointer h-min border-b-primary-500 hover:border-b-2 transition-border w-max"
+>
+	{#if icon != ''}
+		<!-- Icon Component -->
+	{/if}
+	<slot />
+</a>
