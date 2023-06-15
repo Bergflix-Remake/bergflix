e7bd5e9368f6f21bcab4534ddef097d841587fdd
Leo P <anniken@bergflix.de>
2023-06-14 22:57:17 -0300

    feat(components): add Button and Link components
    - Add ButtonLink.svelte with link and button functionality
    - Add Link.svelte with simple link styling and icon support
    
    ⚠️ Generated by a development version of GCM-AI.
    Results may be inaccurate.

---
+++ src/components/Link/ButtonLink.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	export let href: string;
+	export let icon = '';
+	export let variant = 'variant-filled-primary';
+
+	$: isExternal = href.includes('http');
+</script>
+
+<a
+	{href}
+	target={isExternal ? '_blank' : undefined}
+	rel={isExternal ? 'noopener noreferrer' : undefined}
+	class="btn {variant} btn-sm flex-row items-center"
+>
+	<span class="grid place-items-center">
+		{#if icon}
+			<!-- Icon Component -->
+			<Icon icon={`mingcute:${icon}`} class="inline w-5 h-5" />
+		{:else if isExternal}
+			<!-- External Link Icon -->
+			<Icon icon={'mingcute:external-link-line'} />
+		{/if}
+	</span>
+	<span>
+		<slot />
+	</span>
+</a>
