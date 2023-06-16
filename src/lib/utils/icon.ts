35521d7143d0d412ecd9653375f1d7123c4548c1
Leo P <anniken@bergflix.de>
2023-06-16 15:10:43 -0300

    feat(utils): `iconpack()` now supports iconpack

--- src/lib/utils/icon.ts
+++ src/lib/utils/icon.ts
@@ -1,4 +1,10 @@
-export const iconpack = (name: string) => {
+/**
+ * Checks for iconpack prefix and adds it if missing.
+ * @param name icon name, with or without iconpack prefix
+ * @param iconpack iconpack name, defaults to `mingcute`
+ * @returns `''` if name is nullish, otherwise the icon name with iconpack prefix
+ */
+export const iconpack = (name: string, iconpack?: string) => {
 	if (!name) return '';
-	return name.includes(':') ? name : `mingcute:${name}`;
+	return name.includes(':') ? name : `${iconpack || 'mingcute'}:${name}`;
 };
