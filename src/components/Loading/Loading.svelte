dbccfdedeaaca33df8e7b2ef430d951fe305a8c9
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2023-06-17 00:59:12 +0200

    feat(components): create Loading Component

---
+++ src/components/Loading/Loading.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let animation = 'bricks';
+	const animationList: readonly string[] = [
+		'elastic',
+		'pulse',
+		'flashing',
+		'collision',
+		'revolution',
+		'carousel',
+		'typing',
+		'windmill',
+		'bricks',
+		'floating',
+		'fire',
+		'spin',
+		'falling',
+		'stretching'
+	];
+
+	animation = animationList.includes(animation) ? animation : 'bricks';
+</script>
+
+<div class="relative">
+	<div class={`dot-${animation}`} />
+</div>
