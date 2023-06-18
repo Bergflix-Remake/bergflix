7dc57d8d5049ebf326e56251751262bbe458a999
Leo P <anniken@bergflix.de>
2023-06-18 17:22:07 -0300

    feat(components): state prop for poster

--- src/components/Poster/Poster.svelte
+++ src/components/Poster/Poster.svelte
@@ -3,25 +3,30 @@
 	import Icon from '@iconify/svelte';
 	import Logo from '../Logo.svelte';
 	import Loading from '../Loading/Loading.svelte';
+	import { t } from '$lib/translations';
 
 	export let url: string;
 	export let original: boolean;
-	export let loading = false;
-	export let error = false;
+	export let state: { state: 'loading' | 'error' | 'success'; message?: string } = {
+		state: 'success'
+	};
 </script>
 
 <div
 	class="relative flex flex-col items-center justify-center flex-shrink-0 overflow-hidden text-center text-white transition-all duration-200 bg-center bg-cover rounded-md shadow-md cursor-pointer hover:-translate-y-1 group aspect-video bg-clean-dark-800 w-80 hover:shadow-2xl hover:shadow-primary-500/10"
 >
-	{#if error}
+	{#if state.state === 'error'}
 		<div class="flex flex-col items-center justify-center text-red-500">
 			<Icon icon={iconpack('alert-fill')} class="w-10 h-10" />
-			<p>Es gab einen Fehler. Bitte versuche es nochmal.</p>
+			<p>{state.message ?? $t('common.errorCodes.default.description')}</p>
 		</div>
-	{:else if loading}
-		<Loading />
+	{:else if state.state === 'loading'}
+		<div class="flex flex-col items-center justify-center text-red-500">
+			<Loading />
+			<p>{state.message}</p>
+		</div>
 	{:else}
-		<img src={url} alt="Thumbnail of..." />
+		<img src={url} alt={state.message ?? 'Thumbnail'} />
 		{#if original}
 			<div class="absolute flex top-2 right-2">
 				<Logo className="!text-base">Original</Logo>
